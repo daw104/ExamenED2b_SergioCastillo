0d2940483012c5818804dbaf120a0d61f0cc3363
SergioCastillo <daw04.2022@gmail.com>
2022-03-07 12:55:39 +0100

    Proyecto Documentado

--- src/examen/Hora.java
+++ src/examen/Hora.java
@@ -1,8 +1,21 @@
 package examen;
-
+/**
+ * Clase Hora que verificara la hora
+ * @author Sergio Castillo DAW104
+ *@version 1.0
+ */
 public class Hora {
+	/**
+	 * variable booleana horaValida
+	 */
 	public static boolean horaValida;
-
+		/**
+		 * Metodo validarHora que valida si la hora esta bien o no con un booleano
+		 * @param segundos comprueba los segundos
+		 * @param minutos comprueba los minutos
+		 * @param horas comprueba las horas
+		 * @return True si la hora esta bien, False en caso contraria
+		 */
 	public static boolean validarHora(int segundos, int minutos, int horas) {
 		horaValida = true;
 		if ((horas >= 0) && (minutos >= 0) && (segundos >= 0)) {
